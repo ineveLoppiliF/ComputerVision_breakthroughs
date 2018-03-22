896f859062a129df1027b4533cad3ad8ba598e57
Unknown <gioo27_10@tiscali.it>
2018-03-22 17:29:24 +0100

    Area check

--- python/functions/__init__.py
+++ python/functions/__init__.py
@@ -2,4 +2,5 @@ from .index_to_eliminate import index_to_eliminate
 from .out_area_ratio import out_area_ratio
 from .out_points_ratio import out_points_ratio
 from .remove_temporarily_matches import remove_temporarily_matches
+from .validate_area import validate_area
 from .self_similar_and_fingerprints import *
