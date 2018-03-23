dcd459b37cc98c8ea82c699b998a3c476fc6e3b6
Unknown <filippo.leveni@mail.polimi.it>
2018-03-23 15:06:59 +0100

    Little changes

--- python/functions/validate_area.py
+++ python/functions/validate_area.py
@@ -1,28 +1,16 @@
-from scipy.stats import norm,t
+from scipy.stats import norm
 
 def validate_area(ALPHA, areas, area):
     
-    if len(areas) < 3: return True
-    t_areas_parameters = norm.fit(areas) # returned a list of three parameters (shape=parameters[0], mean=parameters[1] and std=parameters[2])
-    areas_quantiles = norm.interval(ALPHA,t_areas_parameters[0],t_areas_parameters[1])
+    if len(areas) < 2: return True
+    norm_areas_parameters = norm.fit(areas) # returned a list of two parameters (mean=parameters[0] and std=parameters[1])
+    areas_quantiles = norm.interval(ALPHA,norm_areas_parameters[0],norm_areas_parameters[1])
     print('-----')
     print('Area: '+str(areas))
-    print('Mean: '+str(t_areas_parameters[0]))
-    print('Std: '+str(t_areas_parameters[1]))
+    print('Mean: '+str(norm_areas_parameters[0]))
+    print('Std: '+str(norm_areas_parameters[1]))
     print(str(areas_quantiles[0])+' < '+str(area)+' < '+str(areas_quantiles[1]))
-    print('-----')
-    
-    
-    
-    t_areas_parameters = t.fit(areas) # returned a list of three parameters (shape=parameters[0], mean=parameters[1] and std=parameters[2])
-    areas_quantiles1 = t.interval(ALPHA,t_areas_parameters[0],t_areas_parameters[1],t_areas_parameters[2])
-    print('-----')
-    print('Area: '+str(areas))
-    print('Mean: '+str(t_areas_parameters[1]))
-    print('Std: '+str(t_areas_parameters[2]))
-    print(str(areas_quantiles1[0])+' < '+str(area)+' < '+str(areas_quantiles1[1]))
-    print('-----')
-    
+    print('-----')  
     
     
     if area >= areas_quantiles[0] and area <= areas_quantiles[1]: return True
