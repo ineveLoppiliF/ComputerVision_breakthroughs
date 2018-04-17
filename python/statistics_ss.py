1b5c007a9f562f39088020ec9a306433efb0caba
valeionny <10452850@polimi.it>
2018-04-17 18:35:01 +0200

    statistics

--- python/statistics_ss.py
+++ python/statistics_ss.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 from match_homography_excel_version import match_homography_excel_version
 
-templates=['../data/images/template/template_twinings.jpg','../data/images/template/template_twinings.jpg']
-tests=['../data/images/test/twinings5.jpg','../data/images/test/twinings1.jpg']
+templates=['../data/images/template/mickey.jpeg','../data/images/template/lipton_front.jpg','../data/images/template/template_twinings.jpg']
+tests=['../data/images/test/find_mouse_hard.jpeg','../data/images/test/lipton_back_ordered.jpg','../data/images/test/twinings1.jpg']
 
+self_similar_perc=[]
+tot_matches=[]
 for i in range(len(tests)):
-    match_homography_excel_version(templates[i],tests[i])
-    
+    (ss,tm)=match_homography_excel_version(templates[i],tests[i])
+    self_similar_perc.append(ss)
+    tot_matches.append(tm)
 
-plt.plot([1,2,3,4], [1,4,9,16],'ro')
-plt.axis([0, 6, 0, 20])
+plt.plot(tot_matches, self_similar_perc,'ro')
+##plt.axis([0, 6, 0, 20])
 plt.show()
